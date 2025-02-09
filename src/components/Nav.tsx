b4bb9c930f8fe153693346b1132621fe80507f65
Ace <matt.aceves@yahoo.com>
2025-02-08 20:43:08 -0700

    updated code

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,18 @@
+import { Link } from "react-router-dom";
+
 const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <div>Nav</div>
+    <nav className="nav">
+      <ul className="nav">
+        <li className="nav-item nav-link">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="nav-item nav-link">
+          <Link to="/SavedCandidates">Potential Candidates</Link>
+        </li>
+      </ul>
+    </nav>
   )
 };
 
--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,13 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+export default interface Candidate {
+  name: string;
+  userName: string;
+  location: string;
+  avatar: string;
+  email: string;
+  html_url: string;
+  company: string;
+  bio: string;
+
+}
+
