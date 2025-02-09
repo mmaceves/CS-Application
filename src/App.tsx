52f04a664beeaba9db10080b7be7a56e0f299f97
Ace <matt.aceves@yahoo.com>
2025-02-08 23:37:35 -0700

    fixed app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import Nav from './components/Nav';
 import type Candidate from './interfaces/Candidate.interface';
