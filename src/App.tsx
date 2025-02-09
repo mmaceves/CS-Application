2eb726f54214411273c7e8a70c554df62649c0a7
Ace <matt.aceves@yahoo.com>
2025-02-08 20:45:02 -0700

    updated app and main files

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
+import React, { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import Nav from './components/Nav';
+import type Candidate from './interfaces/Candidate.interface';
 
-function App() {
+const App = () => {
+  const [potentialCandidates, setPotentialCandidates] = useState<Candidate[]>([]);
+
   return (
     <>
       <Nav />
       <main>
-        <Outlet />
+        <Outlet context={{ potentialCandidates, setPotentialCandidates }} />
       </main>
     </>
   );
-}
+};
 
 export default App;
